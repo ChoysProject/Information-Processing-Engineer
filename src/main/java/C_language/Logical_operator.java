687578c104cff027f79682f0c2f99ee3fca7ac10
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-23 17:52:42 +0900

    8강 논리연산 증감연산자

---
+++ src/main/java/C_language/Logical_operator.java
@@ -0,0 +1,47 @@
+package C_language;
+
+public class Logical_operator {
+
+	
+	
+
+//	8강 논리연산자, 증감연산
+//	
+//	a 8
+//	b 1
+//	c 0
+//	ap 0
+//	bp 0
+//	
+//	a 8
+//	b 1
+//	c 0
+//	ap 8
+//	bp 0
+//	
+//	a 9
+//	b 1
+//	c 0
+//	ap 8
+//	bp 0
+//	
+//	a 9
+//	b 2
+//	c 0
+//	ap 8
+//	bp 2
+//	
+//	a 9
+//	b 3
+//	c 0
+//	ap 8
+//	bp 2
+//	
+//	a 9
+//	b 3
+//	c 0
+//	ap 8
+//	bp 2
+	
+	
+}
