2e702f8014c60ac9dc463ec1029c2bf5f392e5db
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-23 18:09:52 +0900

    9강 산술연산자.

---
+++ src/main/java/C_language/Arithmetic_operations_02.java
@@ -0,0 +1,14 @@
+package C_language;
+
+public class Arithmetic_operations_02 {
+
+//	9강 산술연산자
+//
+//	y = 3 + 2 * (x = 7 / 2);
+//
+//	답은
+//	x = 3 , y = 9
+//	우선순위를 잘보고 공부할것.
+//	같은 우선순위일 경우는 앞에서부터 처리할 것.
+	
+}
