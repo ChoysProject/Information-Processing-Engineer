72127eb2a040edeb996af12d0ac462c7cf9fece0
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-23 16:44:39 +0900

    6강 연산자와 우선순위

---
+++ src/main/java/C_language/Operators_and_Priorities.java
@@ -0,0 +1,32 @@
+package C_language;
+
+public class Operators_and_Priorities {
+
+	
+	
+//	6강 연산자 우선순위
+//
+//
+//	높음
+//	()
+//	++ -- ~ !
+//	* / %
+//	+ -
+//	<< >>
+//	< <= > >=
+//	== !=
+//	& ^ |
+//	&& ||
+//	낮음
+//
+//	int a = 5, b = 5;
+//
+//	a*= 3 + b++;
+//	printf("%d %d", a, b);
+//	return 0;
+//
+//	답 a 40 b 6
+	
+	
+	
+}
