a577ac45eb51cb1fbf3157f0c64587d1638a8df4
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-25 16:54:15 +0900

    16강 while문 완료

---
+++ src/main/java/C_language/While.java
@@ -0,0 +1,24 @@
+package C_language;
+
+public class While {
+
+	
+
+//	16강 while 문
+//	
+//	    int i = 0
+//	    
+//	    while(i < 3)
+//	    {
+//	        printf("i : %d\n", i);
+//	        i++
+//	
+//	    }
+//	
+//	    printf("sum=:%d\n", i);
+//	
+//	    답 : 3
+	    	
+	
+	
+}
