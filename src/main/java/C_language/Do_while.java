f731199cee15a8e8cf9ed603181486b5a214fe29
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-25 17:12:54 +0900

    17강 do_while 완료

---
+++ src/main/java/C_language/Do_while.java
@@ -0,0 +1,27 @@
+package C_language;
+
+public class Do_while {
+
+	
+
+
+//	17강 do~while문, for, while문
+//	
+//	
+//	
+//	    do~while 구조
+//	
+//	    do{
+//	        수행하는 작업들
+//	    }while(조건)
+//	
+//	
+//	    int i=3;
+//	    do{
+//	        printf("i=%d\n",i);
+//	        i++;
+//	    }while(i<3);
+//	    printf("sum=%d\n",i);
+	
+	
+}
