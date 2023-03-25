b57ed29190577af2285f624acb5fecdd651a6653
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-25 16:25:49 +0900

    15강 반복문 for 완료

---
+++ src/main/java/C_language/For.java
@@ -0,0 +1,32 @@
+package C_language;
+
+public class For {
+
+	
+
+//    15강 반복문, for문
+//
+//
+//        for
+//            - 정해진 횟수만큼 반복
+//        
+//        while
+//            - 조건이 만족하는 동안 반복
+//
+//        do ~ while
+//            - 무조건 한번 수행 후 조건이 만족하는 동안 반복
+//        
+//
+//    int j;
+//    int sum = 0;
+//
+//    for(j=2; j<= 70; j+=5)
+//
+//    sum = sum +1;
+//    printf("%d", sum)
+//
+//    답 j = 72, sum = 14
+
+
+	
+}
