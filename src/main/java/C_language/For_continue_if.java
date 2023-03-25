0d8581544bf62458bcb0254b952605ca9effac9e
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-25 17:46:35 +0900

    19강 for문, continue, break, if문 완료

---
+++ src/main/java/C_language/For_continue_if.java
@@ -0,0 +1,30 @@
+package C_language;
+
+public class For_continue_if {
+
+
+
+
+//	19강 for문, continue, break, if문
+//	
+//	
+//	    반복문 옵션
+//	    continue
+//	        - 더이상 아래 문장을 실행하지 않고, 반복문 처음으로 돌아가게함.
+//	
+//	
+//	
+//	    int i, sum =0;
+//	
+//	    for(i =1;i<=10; i+=2){
+//	        if(i%2 && i%3) continue;
+//	        sum+= i;
+//	    }
+//	    printf("%d",sum);
+//	    return 0;
+//	
+//	    답은 12
+	
+	
+	
+}
