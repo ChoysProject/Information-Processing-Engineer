0e33aa24b9e96c4905dcdae02da97fea969585e8
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-25 17:25:35 +0900

    18강 while, if 문 완료

---
+++ src/main/java/C_language/While_if.java
@@ -0,0 +1,21 @@
+package C_language;
+
+public class While_if {
+
+	
+//	18강 while, if문
+//
+//    int a=120, b=45;
+//
+//    while(a != b)
+//    {
+//        if(a > b)a = a - b;
+//        else b = b - a;
+//    }
+//    printf("%d",a);
+//
+//    a = 15
+//    b = 15
+    
+	
+}
