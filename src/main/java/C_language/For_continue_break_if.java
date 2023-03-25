e9d3fd14013dba12c780f950c3759d11e4bb88bf
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-25 18:25:10 +0900

    20강 다중 for, if문, continue, break; 완료

---
+++ src/main/java/C_language/For_continue_break_if.java
@@ -0,0 +1,35 @@
+package C_language;
+
+public class For_continue_break_if {
+
+
+//	20강 다중 for, if문, continue, break;
+//	
+//	
+//	
+//	    int i, j, sum=0;
+//	
+//	    for(i=1;i<10;i++){
+//	        for(j=1;j<10;j++){
+//	            if(j%3==0) continue;
+//	            if(i%4) break;
+//	            sum++;
+//	        }
+//	    }
+//	
+//	    답은 42다;
+//	
+//	    j=6씩 올라가고 continue 조건은 3 6 9 반복때 sum++이 올라가지않는다.
+//	
+//	    i반복 break는 4 8 에는 갯수를 세지말아라
+//	
+//	    j반복 = 1 2 4 5 7 8;
+//	        6번 sum ++
+//	    i반복 = 1 2 3 5 6 7 9;
+//	        7번 6씩 계산함
+//	
+//	        답은 6*7 = 42이다.
+	        
+	
+	
+}
