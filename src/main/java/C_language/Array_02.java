2390a3ddbf89e41b067c433fc725d505ec04488e
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-27 22:35:02 +0900

    22강 배열 2 완료

---
+++ src/main/java/C_language/Array_02.java
@@ -0,0 +1,28 @@
+package C_language;
+
+public class Array_02 {
+
+	
+
+//
+//	22강 배열 2
+//
+//
+//	void main(){
+//	
+//	    int i;
+//	    char ch;
+//	    char str[7] = "nation";
+//	    for(i = 0; i< 4; i++){
+//	        ch = str[5-1];
+//	        str[5-i] = str[i];
+//	        str[i] = ch;
+//	    }
+//	    printf("%s\n", str);
+//	    return 0;
+//	}
+//	
+//	답 notian
+
+	
+}
