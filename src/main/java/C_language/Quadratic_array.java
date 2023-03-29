64e326cab2bb821f54bc32698bd99ca0ccc33dd5
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-29 21:22:44 +0900

    29강 2차원 배열과 프린터 완료 ~

---
+++ src/main/java/C_language/Quadratic_array.java
@@ -0,0 +1,26 @@
+package C_language;
+
+public class Quadratic_array {
+
+	
+	
+//	29강 2차원 배열과 프린터
+//
+//    int a[3][2] = {{1,2},{3,4},{5,6}};
+//
+//    int main (){
+//
+//        int data[][3] = {1,3,2,5,2,9,6,8,7};
+//        int *p = data[1];
+//        int x, y;
+//        x =*p;
+//        y = *(p+2);
+//        printf("x=%d,y=%d\n", x,y);
+//    }
+//
+//    x = 5, y = 9;
+
+    
+
+
+}
