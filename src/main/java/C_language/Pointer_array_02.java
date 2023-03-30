3a79dede91f91ce73897426f878fff3c7672ba26
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-30 21:24:57 +0900

    32강 완료!

---
+++ src/main/java/C_language/Pointer_array_02.java
@@ -0,0 +1,18 @@
+package C_language;
+
+public class Pointer_array_02 {
+
+	
+
+
+//32강 포인터 배열 2
+//
+//	char *array1[2] = {"Good morning", "C language"};
+//	
+//	printf("%s",array1[0]+5);
+//	printf("%s",array1[0]+6);
+//	
+//	답 morning
+//	답 u
+
+}
