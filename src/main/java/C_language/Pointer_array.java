19dbbb8aad3455ef6f3578465c811617c336b3e3
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-30 21:27:39 +0900

    33강 포인터 배열 3 완료

---
+++ src/main/java/C_language/Pointer_array.java
@@ -0,0 +1,22 @@
+package C_language;
+
+public class Pointer_array {
+
+	
+
+
+
+//33강 포인터 배열 3
+//
+//    int a[2][3] = {{-3, 14, 5}{1,-10,8}}
+//    int *b[] = {a[0], a[1]}
+//    int *p = b[1]
+//
+//    printf("%d", *b[1])
+//    printf("%d", *(++p))
+//    printf("%d", *[--p-2])
+//
+//    
+	
+	
+}
