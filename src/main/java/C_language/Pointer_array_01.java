d892b23651a25df8bf0a938ef1f761d1279a7197
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-03-30 21:23:08 +0900

    31강 포인터 배열 1 완료

---
+++ src/main/java/C_language/Pointer_array_01.java
@@ -0,0 +1,23 @@
+package C_language;
+
+public class Pointer_array_01 {
+
+	
+
+
+
+//31강 포인터 배열 1
+//
+//포인터 배열 
+//    - 배열의 요소가 포인터 (메모리 주소)로 이루어진 것.
+//
+//    int main(){
+//
+//        static char *c[] = {"aaa", "bbb", "ccc"}
+//        printf("%s",*(c+1));
+//    }
+//
+//    답 bbb
+//
+
+}
