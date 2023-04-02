9ecdd6f6ffa723d0eb56d3cded1ee51ebef6492e
조영상 <96724136+ChoysProject@users.noreply.github.com>
2023-04-02 14:35:07 +0900

    34강 포인터 배열 4 완료 ~

---
+++ src/main/java/C_language/Pointer_array_04.java
@@ -0,0 +1,21 @@
+package C_language;
+
+public class Pointer_array_04 {
+
+	
+
+
+//34강 포인터 배열 4
+//
+//int *arr[3]
+//
+//int a = 12, b =24, c= 36
+//
+//arr[0] = &a
+//arr[1] = &b
+//arr[2] = &C
+//
+//printf("%d", arr[1] + **arr+1);
+//
+
+}
